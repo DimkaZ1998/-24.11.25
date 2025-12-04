6859cc4c35baeb771b99dd61058fc576ef2be3fc
DimkaZ <krepish1975@gmail.com>
2025-12-04 13:36:41 +0300

    решение задачи №1

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const text = document.getElementById('text-element');
+const originalText = document.getElementById('text-element').textContent;
+function changeText(){
+    text.textContent =  'Текст был изменен!';
+}
+
+function addText(){
+    text.textContent += '(дополнено)';
+}
+
+function resetText(){
+    text.textContent = originalText;
+}
